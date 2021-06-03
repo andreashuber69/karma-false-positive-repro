77ea751e7b03eed921d940c4b8eb07213c6a0f71
Andreas Huber <andreashuber69@users.noreply.github.com>
2021-06-03 17:35:58 +0200

    chore: remove unnecessary code

--- TaskQueue.spec.ts
+++ TaskQueue.spec.ts
@@ -10,59 +10,26 @@ const randomDelay = async () => await new Promise<number>(
 
 const throwException = async () => await Promise.reject(new Error("Operation failed."));
 
-const precision = -2;
-
-const expectFailure = async (promise: Promise<never>) => {
-    try {
-        await promise;
-        fail("The task unexpectedly succeeded.");
-    } catch (e: unknown) {
-        if (e instanceof Error) {
-            expect(e.message).toEqual("Operation failed.");
-        } else {
-            fail("Unexpected exception type.");
-        }
-    }
-};
-
 describe(TaskQueue.name, () => {
-    describe("queue", () => {
-        it("should execute tasks sequentially", async () => {
-            const sut = new TaskQueue();
-            const start = Date.now();
-            const firstPromise = sut.queue(randomDelay);
-            const secondPromise = sut.queue(randomDelay);
-            const firstDelay = await firstPromise;
-            expect(Date.now() - start).toBeCloseTo(firstDelay, precision);
-
-            const thirdPromise = sut.queue(throwException);
-            const fourthPromise = sut.queue(randomDelay);
-            const secondDelay = await secondPromise;
-            expect(Date.now() - start).toBeCloseTo(firstDelay + secondDelay, precision);
-
-            await expectFailure(thirdPromise);
-            expect(Date.now() - start).toBeCloseTo(firstDelay + secondDelay, precision);
-
-            const fourthDelay = await fourthPromise;
-            expect(Date.now() - start).toBeCloseTo(firstDelay + secondDelay + fourthDelay, precision);
-        });
-    });
-
     describe("idle", () => {
         it("should only complete when all tasks have completed", async () => {
             const sut = new TaskQueue();
-            const start = Date.now();
-            const firstPromise = sut.queue(randomDelay);
-            const secondPromise = sut.queue(randomDelay);
-            const thirdPromise = sut.queue(throwException);
-            const fourthPromise = sut.queue(randomDelay);
+            const rejectingPromise = sut.queue(throwException);
+            const resolvingPromise = sut.queue(randomDelay);
             await sut.idle();
-            const actualTotalDelay = Date.now() - start;
-            await expectFailure(thirdPromise);
-            const totalDelay = await firstPromise + await secondPromise + await fourthPromise;
-            // Make sure the previous statement did not further delay, i.e. all tasks have in fact completed.
-            expect(Date.now() - start).toBeCloseTo(actualTotalDelay, precision);
-            expect(actualTotalDelay).toBeCloseTo(totalDelay, precision);
+
+            try {
+                await rejectingPromise;
+                fail("The task unexpectedly succeeded.");
+            } catch (e: unknown) {
+                if (e instanceof Error) {
+                    expect(e.message).toEqual("Operation failed.");
+                } else {
+                    fail("Unexpected exception type.");
+                }
+            }
+
+            console.log(await resolvingPromise);
         });
     });
 });
