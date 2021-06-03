972cff841b2f0821cef4a15c4c79ea9847d3e76c
Andreas Huber <andreashuber69@users.noreply.github.com>
2021-06-03 20:46:56 +0200

    chore: simplify test

--- TaskQueue.spec.ts
+++ TaskQueue.spec.ts
@@ -5,26 +5,24 @@ const delay = async () => await new Promise<void>((resolve) => setTimeout(resolv
 
 const throwException = async () => await Promise.reject(new Error("Operation failed."));
 
-describe(TaskQueue.name, () => {
-    describe("idle", () => {
-        it("should only complete when all tasks have completed", async () => {
-            const sut = new TaskQueue();
-            const rejectingPromise = sut.queue(throwException);
-            const resolvingPromise = sut.queue(delay);
-            await sut.idle();
+describe("", () => {
+    it("", async () => {
+        const sut = new TaskQueue();
+        const rejectingPromise = sut.queue(throwException);
+        const resolvingPromise = sut.queue(delay);
+        await sut.idle();
 
-            try {
-                await rejectingPromise;
-                fail("The task unexpectedly succeeded.");
-            } catch (e: unknown) {
-                if (e instanceof Error) {
-                    expect(e.message).toEqual("Operation failed.");
-                } else {
-                    fail("Unexpected exception type.");
-                }
+        try {
+            await rejectingPromise;
+            fail("The task unexpectedly succeeded.");
+        } catch (e: unknown) {
+            if (e instanceof Error) {
+                expect(e.message).toEqual("Operation failed.");
+            } else {
+                fail("Unexpected exception type.");
             }
+        }
 
-            await resolvingPromise;
-        });
+        await resolvingPromise;
     });
 });
