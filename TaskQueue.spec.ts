443c4ca875ae38f72bd3dc47b4b283a72dbe758b
Andreas Huber <andreashuber69@users.noreply.github.com>
2021-06-06 17:57:14 +0200

    refactor: remove TaskQueue

--- TaskQueue.spec.ts
+++ TaskQueue.spec.ts
@@ -1,28 +1,7 @@
-class TaskQueue {
-    public async queue<T>(createNext: () => Promise<T>) {
-        this.current = this.executeAfterCurrent(createNext);
-        return await this.current;
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    private current: Promise<unknown> = Promise.resolve();
-
-    private async executeAfterCurrent<T>(createNext: () => Promise<T>) {
-        try {
-            await this.current;
-        } catch {
-        }
-
-        return await createNext();
-    }
-}
-
 describe("", () => {
     it("", async () => {
-        const sut = new TaskQueue();
-        const rejectingPromise = sut.queue(() => Promise.reject(new Error("Operation failed.")));
-        const resolvingPromise = sut.queue(() => new Promise<void>((resolve) => setTimeout(resolve, 100)));
+        const rejectingPromise = Promise.reject(new Error("Operation failed."));
+        const resolvingPromise = new Promise<void>((resolve) => setTimeout(resolve, 100));
 
         await resolvingPromise;
 
